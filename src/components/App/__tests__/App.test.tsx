1f8c969a71a56d81dde94e624a2c4bccb4dadce7
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2022-11-26 10:37:10 -0600

    ➕ add test config

---
+++ src/components/App/__tests__/App.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import App from '..';
+
+describe('App', () => {
+  it('renders without crashing', () => {
+    render(<App />);
+    screen.debug();
+  });
+});
