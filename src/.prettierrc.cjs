35af63ba82bfcd31c41a583e2a4e2c98ddaab6b6
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2022-11-26 10:18:24 -0600

    ➕ add prettier config

---
+++ src/.prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    trailingComma: 'es5',
+    tabWidth: 2,
+    semi: true,
+    singleQuote: true,
+    printWidth: 120,
+    bracketSpacing: true,
+    arrowParens: 'always',
+    endOfLine: 'lf',
+};
